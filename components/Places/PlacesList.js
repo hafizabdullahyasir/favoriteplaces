19feaed25fb932940b410092ae02b5038c4044e8
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-27 12:51:49 +0500

    created component

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+
+
+export default function PlacesList() {
+    return(
+        <View>
+            <Text>Places List</Text>
+        </View>
+    )
+}
