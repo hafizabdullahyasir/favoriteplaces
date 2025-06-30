bd88cc592b8709efd96b3c6c6dd5716f5bcd7e8f
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-30 11:17:37 +0500

    added place item component to show the list

---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,20 @@
+import { View, Text, Image, Pressable, StyleSheet } from "react-native";
+
+export default function PlaceItem({place, onSelect}) {
+return (
+  <Pressable onPress={onSelect}>
+      <View>
+        <Image source={{uri: place.imageUri}}/>
+        <View>
+            <Text>{place.title}</Text>
+            <Text>{place.address}</Text>
+        </View>
+    </View>
+  </Pressable>
+
+)
+
+
+ 
+
+}
