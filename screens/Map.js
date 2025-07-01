1ccd41467cfa7cf7765cbe4bfae8004b41faab81
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-07-01 16:59:49 +0500

    updated map and added marker

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,10 @@
 import { View, Text, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-
+import { useState } from "react";
 
 export default function Map() {
+
+    const [selectedLocation, setSelectedLocation] = useState(null);
 
 
     const region = {
@@ -12,13 +14,20 @@ export default function Map() {
         longitudeDelta: 0.01,
     }
 
+
+    const selectLocationHandler = (event) => {
+        const lat = event.nativeEvent.coordinate.latitude;
+        const lng = event.nativeEvent.coordinate.longitude;
+        setSelectedLocation({ lat: lat, lng: lng });
+    }
+
     return (
-      <MapView initialRegion={region} style={styles.map} showsUserLocation={true}></MapView>
+        <MapView initialRegion={region} style={styles.map} onPress={selectLocationHandler} >{selectedLocation && <Marker title="Picked Location" coordinate={{ latitude: selectedLocation.lat, longitude: selectedLocation.lng}} />}</MapView>
     );
 }
 
 const styles = StyleSheet.create({
- map: {
-    flex: 1,
- }
+    map: {
+        flex: 1,
+    }
 });
