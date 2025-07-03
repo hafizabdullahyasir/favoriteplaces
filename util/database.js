1363c13baecbe833a80185dcbd9f0cfb04141f67
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-07-03 16:08:01 +0500

    created database in utils and setup and functionality

---
+++ util/database.js
@@ -0,0 +1,46 @@
+import * as SQLite from 'expo-sqlite';
+
+const database = SQLite.openDatabaseAsync('places.db');
+
+
+export function init() {
+    const promise = new Promise((resolve, reject) => {
+        database.transaction((tx) => {
+            tx.executeSql(`
+            CREATE TABLE IF NOT EXISTS places(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                imageUri TEXT NOT NULL,
+                address TEXT NOT NULL,
+                lat REAL NOT NULL,
+                lng REAL NOT NULL
+            ) `,
+            [],
+                () => { resolve() },
+                (_, error) => { reject(error) },
+
+
+
+            )
+
+
+
+
+        })
+    })
+    return promise;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
